9ab145d23d694764b8d5570fb15f720f15e24b35
Khairul Hidayat <me@khairul.my.id>
2024-08-11 03:16:31 +0700

    feat: hide appicon & appname, place on bottom

--- js/UI/Home/Home.js
+++ js/UI/Home/Home.js
@@ -1,53 +1,52 @@
-import App from "../../App"
-import { showPage } from "../../Utils/DOMUtils"
-
+import App from "../../App";
+import { showPage } from "../../Utils/DOMUtils";
 
 export default class Home {
-	constructor() {
-		this.app = new App()
-		this.config = this.app.config
-		this.init()
-	}
+  constructor() {
+    this.app = new App();
+    this.config = this.app.config;
+    this.init();
+  }
 
-	init() {
-		this.initButtons()
-		this.initHomeUI()
-		this.initBackButtons()
-	}
+  init() {
+    this.initButtons();
+    // this.initHomeUI();
+    this.initBackButtons();
+  }
 
-	initButtons() {
-		let buttons = document.querySelector(".buttons").children
-		for (let button of buttons) {
-			let target = button.getAttribute("t")
-			if (target) {
-				button.addEventListener("click", () => {
-					showPage(target)
-				})
-			}
-		}
-	}
+  initButtons() {
+    let buttons = document.querySelector(".buttons").children;
+    for (let button of buttons) {
+      let target = button.getAttribute("t");
+      if (target) {
+        button.addEventListener("click", () => {
+          showPage(target);
+        });
+      }
+    }
+  }
 
-	initBackButtons() {
-		let backButtons = document.querySelectorAll(".back")
-		for (let button of backButtons) {
-			button.addEventListener("click", () => {
-				showPage("home")
-			})
-		}
-	}
+  initBackButtons() {
+    let backButtons = document.querySelectorAll(".back");
+    for (let button of backButtons) {
+      button.addEventListener("click", () => {
+        showPage("home");
+      });
+    }
+  }
 
-	initHomeUI() {
-		let logo = document.querySelector(".appicon")
-		logo.src = this.config.get("icon")
-		logo.classList.add("notloaded")
-		logo.addEventListener("load", () => {
-			logo.classList.remove("notloaded")
-		})
+  initHomeUI() {
+    let logo = document.querySelector(".appicon");
+    logo.src = this.config.get("icon");
+    logo.classList.add("notloaded");
+    logo.addEventListener("load", () => {
+      logo.classList.remove("notloaded");
+    });
 
-		let name = document.querySelector(".appname")
-		name.innerText = this.config.get("name")
+    let name = document.querySelector(".appname");
+    name.innerText = this.config.get("name");
 
-		let desc = document.querySelector(".appdesc")
-		desc.innerText = this.config.get("desc")
-	}
+    let desc = document.querySelector(".appdesc");
+    desc.innerText = this.config.get("desc");
+  }
 }
